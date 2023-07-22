35b7c5eca008f7755d726cf72e4aaf069fe09db5
kanchanDevi <kanchan.thakurrkr@gmail.com>
2023-07-22 11:33:04 +0530

    changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/global.scss';
+import axios from 'axios';
 
+
+axios.defaults.baseURL = getApiBaseUrl();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api':{
-        target:'http://localhost:8080'
+  // server:{
+  //   proxy:{
+  //     '/api':{
+  //       target:'http://localhost:8080'
 
-      }
-    }
-  }
+  //     }
+  //   }
+  // }
 })
