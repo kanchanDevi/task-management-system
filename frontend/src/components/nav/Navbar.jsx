17f6952fa94783a23a81d6589e43c7a4d9a1c8f2
kanchanDevi <kanchan.thakurrkr@gmail.com>
2023-07-22 16:16:15 +0530

    ch

--- frontend/src/components/nav/Navbar.jsx
+++ frontend/src/components/nav/Navbar.jsx
@@ -1,35 +1,34 @@
-import React, {useState, useEffect} from 'react';
-import classes from './Navbar.module.scss';
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
 import { FaUserAlt } from 'react-icons/fa';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios'
+import { toast } from 'react-hot-toast';
+import { Link } from 'react-router-dom';
+import AuthContext from '../../context/Auth';
+import classes from './Navbar.module.scss';
 
+export default function Navbar() {
+  const [user, setUser] = useState(null);
+  const { verifyAuth } = useContext(AuthContext);
 
+  const getUser = async () => {
+    try {
+      const { data } = await axios.get(`/api/users/me`);
+      setUser(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-const Navbar = () => {
-    const [user, setUser]=useState(null);
-    const nevigate=useNavigate()
-
-const getUser= async()=>{
-try{
-    const {data}= await axios.get('./api/users/me');
-    setUser(data);
-
-}catch(err){
-    console.log(err)
-}
-}
-
-useEffect(() => {
+  useEffect(() => {
     getUser();
   }, []);
 
   const handleLogout = async () => {
     try {
-      await axios.get('/api/auth/logout');
+      await axios.get(`/api/auth/logout`);
       setUser(null);
+      verifyAuth();
       toast.success('Logged out successfully');
-      nevigate('/auth');
     } catch (err) {
       console.log(err);
     }
@@ -39,23 +38,21 @@ useEffect(() => {
 
   return (
     <header>
-    <div className={classes.userInfo}>
-      <FaUserAlt className={classes.userIcon} />
-      <div>
-        <h1 className={classes.name}>{user.name}</h1>
-        <p className={classes.email}>{user.email}</p>
-        <Link to="/edit-profile" className={classes.editBtn}>
-          Edit
-        </Link>
+      <div className={classes.userInfo}>
+        <FaUserAlt className={classes.userIcon} />
+        <div>
+          <h1 className={classes.name}>{user.name}</h1>
+          <p className={classes.email}>{user.email}</p>
+          <Link to="/edit-profile" className={classes.editBtn}>
+            Edit
+          </Link>
+        </div>
       </div>
-    </div>
-    <nav>
-      <button type="button" className={classes.logout} onClick={handleLogout}>
-        logout
-      </button>
-    </nav>
-  </header>
-  )
+      <nav>
+        <button type="button" className={classes.logout} onClick={handleLogout}>
+          logout
+        </button>
+      </nav>
+    </header>
+  );
 }
-
-export default Navbar
