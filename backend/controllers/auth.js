eedb411dad4ec597a6782ead978d596804d899b1
kanchanDevi <kanchan.thakurrkr@gmail.com>
2023-07-22 12:35:20 +0530

    changes

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,76 +1,97 @@
 import bcryptjs from 'bcryptjs';
-import User from '../models/User.js';
 import jwt from 'jsonwebtoken';
-import createError from '../Utils/createError.js';
+import User from '../models/User.js';
+import { createError } from '../utils/error.js';
+
+export const login = async (req, res, next) => {
+  if (!req.body.email || !req.body.password) {
+    return next(
+      createError({
+        message: 'Email and password are required',
+        statusCode: 400,
+      }),
+    );
+  }
+
+  try {
+    const user = await User.findOne({ email: req.body.email }).select(
+      'name email password',
+    );
+    if (!user) {
+      return next(
+        createError({ status: 404, message: 'User not found with the email' }),
+      );
+    }
+    const isPasswordCorrect = await bcryptjs.compare(
+      req.body.password,
+      user.password,
+    );
+    if (!isPasswordCorrect) {
+      return next(
+        createError({ status: 400, message: 'Password is incorrect' }),
+      );
+    }
+    const payload = {
+      id: user._id,
+      name: user.name,
+    };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+    });
+    return res
+      .cookie('access_token', token, {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: process.env.NODE_ENV === 'production',
+      })
+      .status(200)
+      .json({ name: user.name, email: user.email, message: 'login success' });
+  } catch (err) {
+    return next(err);
+  }
+};
 
 export const register = async (req, res, next) => {
-    if (!req.body.name || !req.body.email || !req.body.password){
-        return next(createError({ status: 400, message: 'name, email and password are required' }))
-    }
-
-    try{
-        const salt= await bcryptjs.genSalt(10);
-        const hashedpassword=await bcryptjs.hash(req.body.password, salt);
-
-        const newUser=new User({
-            name:req.body.name,
-            email:req.body.email,
-            password:hashedpassword
-        });
-        await newUser.save();
-        return res.status(201).json("New user created")
-    } catch(err){
-        console.log(err);
-        return next(err);
-    }
-
-}
-
-export const login=async (req, res, next)=>{
-if(!req.body.email || !req.body.password){
-return next(createError({message:'Email and Password are required', status:400}))
-}
-try{
-    const user=await User.findOne({email:req.body.email}).select(
-        'name email password'
+  if (!req.body.name || !req.body.email || !req.body.password) {
+    return next(
+      createError({
+        message: 'Name, Email & password are required',
+        statusCode: 400,
+      }),
     );
-    if(!user){
-        next({message:'User not found with the email', status:404})
-    }
-    const isPasswordCorrect=await bcryptjs.compare(req.body.password, user.password);
-    if(!isPasswordCorrect){
-        next({message:'User not found with the email', status:404})
-    }
-    const payload={
-        id:user._id,
-        name:user.name
-    }
-    const token=jwt.sign(payload, process.env.JWT_SECRET, {
-        expiresIn: '1d',
-    });
-    return res.cookie('access_token', token, {
-        httpOnly:true,
-    }).status(200).json({name: user.name, email: user.email, message: 'login success' })
-}catch(err){
-console.log(err);
-return next(err);
-}
-}
+  }
 
-export const logout=(req, res)=>{
-    res.clearCookie('access_token');
-    return res.status(200).json("logout success")
-}
+  try {
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(req.body.password, salt);
+
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    return res.status(201).json('New User Created');
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export const logout = async (req, res) => {
+  res.clearCookie('access_token');
+  res.status(200).json({ message: 'logout success' });
+};
 
 export const is_logged_in = async (req, res) => {
-    const token = req.cookies.access_token;
-    if (!token) {
+  const token = req.cookies.access_token;
+  if (!token) {
+    return res.json(false);
+  }
+  return jwt.verify(token, process.env.JWT_SECRET, (err) => {
+    if (err) {
       return res.json(false);
     }
-    return jwt.verify(token, process.env.JWT_SECRET, (err) => {
-      if (err) {
-        return res.json(false);
-      }
-      return res.json(true);
-    });
-  };
+    return res.json(true);
+  });
+};
