6b308d656cf4e5a80ff5939ed8d92ee7f1aa6991
kanchanDevi <kanchan.thakurrkr@gmail.com>
2023-07-22 11:16:24 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html'),
+      },
+    },
+  },
   plugins: [react()],
   server:{
     proxy:{
