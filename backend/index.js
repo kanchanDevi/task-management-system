a6b58b8af8734d203fe81e62f5013970ed76db28
kanchanDevi <kanchan.thakurrkr@gmail.com>
2023-07-22 13:07:36 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,57 +1,35 @@
+// app.js (or index.js) - Your main application file
 
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
 import allRoutes from './routes/index.js';
-import dotenv from 'dotenv';
+import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 
 // Load environment variables from .env file
-dotenv.config();
-const app = express();
 
 const PORT = process.env.PORT || 8080;
 const mongoURI = process.env.DB_CONNECTION_STRING;
-const CLIENT_URL_STRING = process.env.CLIENT_URL || 'http://localhost:3000';
 
-const allowedDomains = CLIENT_URL_STRING.split(', ');
-console.log(allowedDomains);
+const app = express();
 
-// middleware
-app.use(
-  cors({
-    credentials: true,
-    origin(origin, callback) {
-      // bypass the requests with no origin (like curl requests, mobile apps, etc )
-      if (!origin) return callback(null, true);
-
-      if (allowedDomains.indexOf(origin) === -1) {
-        const msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  }),
-);
+// Middleware
+app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser());
 
-// routes
+// Routes
 app.use('/api', allRoutes);
 
-// error handler
-app.use((err, req, res) => {
+// Error handling middleware
+app.use((err, req, res, next) => {
   const status = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
-  res.status(status).json({ message, stack: err.stack });
+  return res.status(status).json({ message, stack: err.stack });
 });
-
-
-
-
-
 
 // Connect to MongoDB
 const connectDB = async () => {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/global.scss';
 
-axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
