97d3daa357897316fbda8c02528d0eb48ea719a9
kanchanDevi <kanchan.thakurrkr@gmail.com>
2023-07-22 11:41:34 +0530

    changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/global.scss';
-import axios from 'axios';
 
-
-axios.defaults.baseURL = getApiBaseUrl();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
